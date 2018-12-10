06d67ba5ab4e733b28c56cef5503ec7bfeb72437
Aythami Z. Martin Gonzalez <43406334+Aythamiza@users.noreply.github.com>
2018-12-10 10:34:51 +0000

    Revert "User2.0"

--- docs/travis.md
+++
@@ -1,3 +0,0 @@
-  + Add user in server: travis
-  + give access to carpeta master y develop in server
-  + add keygen in /Users/yourUser/.ssh
--- src/passport/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    auth: {
-      secret: 'Little secret, big responsability...'
-    },
-    database: {
-      local: 'mongodb://localhost/Apiuser20',
-      mLab: '' // if you want to use mLab for example
-    }
-  };
