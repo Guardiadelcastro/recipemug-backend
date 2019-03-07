5ea553f420a6f1bb77e07a55fb58c7b30316d27f
Alejandro Castro <guardiadelcastro@gmail.com>
2019-03-07 11:54:29 +0000

    changed mongoose queries

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -41,7 +41,7 @@ export async function getRecipe(req, res) {
 export async function deleteRecipe(req, res) {
   try {
     const { id } = req.body;
-    const recipe = await Recipe.findByIdAndDelete({ _id: id });
+    const recipe = await Recipe.findOneAndDelete({ _id: id });
     res.json({message: `Recipe ${recipe.title} deleted`});
   } catch (err) {
     res.status(400).json({err});
@@ -50,10 +50,10 @@ export async function deleteRecipe(req, res) {
 export async function updateRecipe(req, res) {
   try {
     const { id, updates} = req.body;
-    const recipe = await Recipe.findByIdAndUpdate(id, {
+    const recipe = await Recipe.findOneAndUpdate({_id: id}, {
       $set: { ...updates, updated: Date.now()}
     })
-    res.json({message: `Recipe updated`, recipe});
+    res.json({message: `Recipe updated`});
   } catch (err) {
     res.status(400).json({err});
   }
