125239ac6fdb9379a7e9fe457f7327d1ddca025b
Alejandro Castro <guardiadelcastro@gmail.com>
2019-04-12 12:11:49 +0100

    moved cors

--- src/server.ts
+++ src/server.ts
@@ -22,12 +22,12 @@ app.set('port', process.env.PORT || 3000);
 
 // middlewares
 // app.use(helmet());
-app.use(morgan('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+
+app.use(cors());
 
 // routes
 app.get('/', (req, res) => res.send('Welcome to the recipemug api'));
