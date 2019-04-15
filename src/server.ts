29086c95eb31d3c74a94e440313923337c9cb2f2
Alejandro Castro <guardiadelcastro@gmail.com>
2019-04-15 10:35:14 +0100

    deleted corsOptions

--- src/server.ts
+++ src/server.ts
@@ -21,17 +21,7 @@ connect(config.mongodb.URI, config.mongodb.options)
 app.set('port', process.env.PORT || 3000);
 
 // middlewares
-const whitelist = ['http://example1.com', 'http://example2.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
