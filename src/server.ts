a31ea3a80466dd155c51c3585da81fae894c7b75
Alejandro Castro <guardiadelcastro@gmail.com>
2019-04-15 09:51:20 +0100

    minor change

--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,8 @@ connect(config.mongodb.URI, config.mongodb.options)
 app.set('port', process.env.PORT || 3000);
 
 // middlewares
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
